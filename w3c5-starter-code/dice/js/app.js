67dc5b6779a7bcbdf81c37bff608906e3ca554c1
Scott Cobban <scobban@gmail.com>
2017-06-12 20:16:11 -0400

    Dice project classwork

---
+++ w3c5-starter-code/dice/js/app.js
@@ -0,0 +1,36 @@
+/*
+
+Creating a page where every time the user hits the "Roll Dice" button, the screen randomly updates the two dice. Use the html and css code included in the starter code folder to get started.
+
+1) Write down pseudocode for the following program.
+
+2) Follow the steps below to write your code.
+* generate a random number between 1 - 6 and store to a variable, random1
+* generate a random number between 1 - 6 and store to a variable, random2
+* combine 'dice-' and random1 to form the random class for the first die, firstDie
+* combine 'dice-' and random2 to form the random class for the second die, secondDie
+* get the first die by ID and update the class to firstDie (hint: document.getElementById)
+* get the first die by ID and update the class to secondDie (hint:document.getElementById)
+
+3) Check to see if the Dice Roll has been hit, if it has run the diceRoll function.
+
+*/
+
+function rollDice(dice1, dice2){
+	var die1 = document.getElementById("first-die");
+	var die2 = document.getElementById("second-die");
+	die1.className = dice1
+	die2.className = dice2
+};
+
+document.getElementById("roll-dice").onclick = function() {
+	var random1 = Math.floor(Math.random() * 6) + 1;
+	var random2 = Math.floor(Math.random() * 6) + 1;
+	var dice1 = "dice-" + random1;
+	var dice2 = "dice-" + random2;
+
+	console.log(random1);
+	console.log(random2);
+
+	rollDice(dice1, dice2);
+}
